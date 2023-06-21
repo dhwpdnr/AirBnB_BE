46d88a86b7b09a2412f3a164b84cb98a648568f1
dhwpdnr <ink1237@gmail.com>
2023-06-21 10:04:38 +0900

    [feat] add field in House model

--- houses/models.py
+++ houses/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class House(models.Model):
     """House Model Definition"""
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
-
+    pets_allowed = models.BooleanField(default=True)
