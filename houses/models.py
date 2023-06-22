1dbaac5e1ca614070d5f011f74d93faab9485daf
dhwpdnr <ink1237@gmail.com>
2023-06-22 13:51:14 +0900

    [test] django documentation

--- houses/models.py
+++ houses/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class House(models.Model):
     """House Model Definition"""
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(verbose_name="Price", help_text="Positive numbers only")
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(default=True, help_text="Does this house allow pet?")
 
     def __str__(self):
         return self.name
