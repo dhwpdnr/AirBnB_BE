b6169fdb0beb0b8c6a3c24b1615b8c5648b95541
dhwpdnr <ink1237@gmail.com>
2023-06-26 09:12:43 +0900

    [feat] add fk field in house model

--- houses/models.py
+++ houses/models.py
@@ -8,6 +8,7 @@ class House(models.Model):
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True, help_text="Does this house allow pet?")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
