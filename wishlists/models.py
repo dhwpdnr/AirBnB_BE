3e68aa29fdc0518260f047a366e0ea45c76332ea
dhwpdnr <ink1237@gmail.com>
2023-07-04 16:37:00 +0900

    [feat] wishlist admin

--- wishlists/models.py
+++ wishlists/models.py
@@ -9,3 +9,6 @@ class Wishlist(CommonModel):
     rooms = models.ManyToManyField("rooms.Room")
     experiences = models.ManyToManyField("experiences.Experience")
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
