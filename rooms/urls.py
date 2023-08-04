ff89abd391dde00a8436e327274eeacdc05db2e8
dhwpdnr <ink1237@gmail.com>
2023-08-04 10:36:43 +0900

    [feat] Amenities api

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import see_all_rooms, see_one_room
+from .views import Amenities, AmenityDetail
 
 urlpatterns = [
-    path("", see_all_rooms),
-    path("<int:room_pk>", see_one_room),
+    path("amenities/", Amenities.as_view()),
+    path("amenities/<int:room_pk>/", AmenityDetail.as_view()),
 ]
