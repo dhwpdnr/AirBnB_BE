c0dc172b235bb2f4facad6773a27e6b77e25c2c4
dhwpdnr <ink1237@gmail.com>
2023-08-11 13:26:31 +0900

    [feat] room amenities

--- rooms/views.py
+++ rooms/views.py
@@ -69,6 +69,13 @@ class Rooms(APIView):
                 except Category.DoesNotExist:
                     raise ParseError("Category not found")
                 room = serializer.save(owner=request.user, category=category)
+                amenities = request.data.get("amenities")
+                for amenity_pk in amenities:
+                    try:
+                        amenity = Amenity.objects.get(pk=amenity_pk)
+                    except Amenity.DoesNotExist:
+                        raise ParseError(f"Amenity with id {amenity_pk} not found")
+                    room.amenities.add(amenity)
                 serializer = RoomDetailSerializer(room)
                 return Response(serializer.data)
             else:
