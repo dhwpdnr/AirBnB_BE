9b96940792cd6f2e5c38281d927266a82aebd7b8
dhwpdnr <ink1237@gmail.com>
2023-08-17 19:46:27 +0900

    [feat] file handle setting

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Amenities, AmenityDetail, Rooms, RoomDetail, RoomReviews
+from .views import Amenities, AmenityDetail, Rooms, RoomDetail, RoomReviews, RoomPhotos
 
 urlpatterns = [
     path("", Rooms.as_view()),
     path("<int:pk>", RoomDetail.as_view()),
     path("<int:pk>/reviews", RoomReviews.as_view()),
+    path("<int:pk>/photos", RoomPhotos.as_view()),
     path("amenities/", Amenities.as_view()),
     path("amenities/<int:room_pk>/", AmenityDetail.as_view()),
 ]
