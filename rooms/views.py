e045213fc7415ce3f1b122341c5b88de59c21ec4
dhwpdnr <ink1237@gmail.com>
2023-08-18 13:10:02 +0900

    [feat] photo uploads

--- rooms/views.py
+++ rooms/views.py
@@ -12,6 +12,7 @@ from rest_framework.exceptions import (
     PermissionDenied,
 )
 from reviews.seriralizers import ReviewSerializer
+from medias.serializers import PhotoSerializer
 
 
 class Amenities(APIView):
@@ -172,5 +173,22 @@ class RoomReviews(APIView):
 
 
 class RoomPhotos(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
     def post(self, request, pk):
-        pass
+        room = self.get_object(pk)
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
