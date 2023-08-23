a45084aa8a9dad7a7ee5ac7b9c8665b932af587f
dhwpdnr <ink1237@gmail.com>
2023-08-23 15:36:57 +0900

    [refactor] category list create api refactor to use generics

--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -12,20 +13,9 @@ class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
 
 
-class Categories(APIView):
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            CategorySerializer(new_category).data
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
+class Categories(generics.ListCreateAPIView):
+    all_categories = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
 class CategoryDetail(APIView):
